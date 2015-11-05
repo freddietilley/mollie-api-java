8d27b1ffc2013499a3051f645ca6ec4f9abd31d3
Freddie Tilley <freddie.tilley@impending.nl>
2015-11-05 17:01:22 +0100

    Add empty body payment test result

--- src/test/java/com/mollie/api/ApiTest.java
+++ src/test/java/com/mollie/api/ApiTest.java
@@ -1,5 +1,9 @@
 package com.mollie.api;
 
+import static org.mockito.Mockito.*;
+
+import java.math.BigDecimal;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,15 +19,29 @@ public class ApiTest {
 
 	@Before
 	public void setUp() throws Exception {
-		api = new MollieClient();
+		api = spy(new MollieClient());
+		api.initResources(api); // make sure the resources also get the spied api
+
 		api.setApiKey(API_KEY);
 	}
 
 	@Test
 	public void testInvalidApiKeyFails() throws MollieException
 	{
+		api = new MollieClient();
 		thrown.expect(MollieException.class);
 		thrown.expectMessage("Invalid api key: \"invalid\". An API key must start with \"test_\" or \"live_\".");
 		api.setApiKey("invalid");
+	}
+
+	@Test
+	public void testCreatePaymentFailsEmptyHttpBody() throws MollieException
+	{
+		thrown.expect(MollieException.class);
+		thrown.expectMessage("Unable to decode Mollie response: \"\"");
+
+		doReturn("").when(api).performHttpCall(MollieClient.HTTP_POST, "payments", "{\"amount\":100,\"redirectUrl\":\"http://www.chucknorris.rhk/return.php\",\"description\":\"Order #1337 24 Roundhousekicks\"}");
+
+		api.payments().create(new BigDecimal(100), "Order #1337 24 Roundhousekicks", "http://www.chucknorris.rhk/return.php", null);
 	}
 }
