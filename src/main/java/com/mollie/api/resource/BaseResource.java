d22ad21747c5a2609ce490dd1208d55e3436e7aa
Freddie Tilley <freddie.tilley@impending.nl>
2015-11-10 11:03:12 +0100

    Use default limit

--- src/main/java/com/mollie/api/resource/BaseResource.java
+++ src/main/java/com/mollie/api/resource/BaseResource.java
@@ -98,7 +98,7 @@ abstract public class BaseResource <T> {
 		}
 	}
 
-	public ArrayList<T> all() throws MollieException { return this.all(0, 0); }
+	public ArrayList<T> all() throws MollieException { return this.all(0, DEFAULT_LIMIT); }
 	public ArrayList<T> all(int offset, int limit) throws MollieException {
 		return this.rest_list(this.getResourceName(), offset, limit);
 	}
